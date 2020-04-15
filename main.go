3e69961daeb7bcde004293fcc873fd022f7ad8e5
Kiryushin Denis <dkiryushin@sedmax.com>
2020-04-15 17:07:52 +0300

    just sum

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+/*
+int sum(int a, int b) {
+	return a+b;
+}
+*/
+import "C"
+import "fmt"
+
+func main() {
+	a:=2
+	b:=3
+	c := C.sum(C.int(a), C.int(b))
+	fmt.Println(c)
+}
