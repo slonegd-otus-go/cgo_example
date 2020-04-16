87fdee1fa2bc79e6c7afabcc74150d82387d8ac3
Kiryushin Denis <dkiryushin@sedmax.com>
2020-04-16 08:28:48 +0300

    c array of struct changed from go

--- main.go
+++ main.go
@@ -8,19 +8,37 @@ typedef struct {
 	int b;
 } some;
 
-void inc(some* v) {
-	v->a++;
-	v->b++;
-}
+some ar[2] = {{1,2}, {3,4}};
+
+some* get_some() { return ar; }
+int   len()      { return 2; }
 */
 import "C"
 import (
 	"fmt"
-) 
+	"unsafe"
+)
 
 func main() {
-	v := C.some{a:1,b:2}
-	C.inc(&v)
-	fmt.Printf("%+v\n", v) // {a:2 b:3}
+	vs := C.get_some()
+	len := C.len()
+	fmt.Printf("%+v\n", vs)        // &{a:1 b:2}
+	fmt.Printf("%+v\n", len)       // 2
+	inc(int(len), vs)
+	fmt.Printf("%+v\n", vs)        // {a:2 b:3}
+	fmt.Printf("%+v\n", get(1,vs)) // {a:4 b:5}
 }
 
+func inc(len int, vs *C.some) {
+	for i := 0; i < len; i++ {
+		v := get(i, vs)
+		v.a++
+		v.b++
+	}
+}
+
+func get(i int, vs *C.some) *C.some {
+	p := uintptr(unsafe.Pointer(vs))
+	p += uintptr(i)*unsafe.Sizeof(*vs)
+	return (*C.some)(unsafe.Pointer(p))
+}
